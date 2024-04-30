ae7e65121ef577bb8172c099df73f3c792a5cb7e
Raghav Vashisht <raghav2011rv@gmail.com>
2024-04-30 17:37:08 +0000

    fix: auto-commit script

--- git-auto-commit.sh
+++ git-auto-commit.sh
@@ -2,13 +2,13 @@
 
 echo "Starting the auto-commit process..."
 
-cd /dramikei/valheim-server/ || exit
+cd /home/dramikei/valheim-server || exit
 
 echo "Adding all changes, including untracked files..."
 git add .
 
 echo "Committing changes..."
-git commit -m "Daily auto-commit on $(date)"
+git commit -m "chore: Daily auto-commit on $(date)"
 
 echo "Pushing to remote..."
 git push origin main
